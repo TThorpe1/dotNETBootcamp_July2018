00225d5b063372d7b81789ca4547af7e1bf73a1c
kamel-rush <kamel@grandcircus.co>
2018-07-23 12:59:21 -0400

    validation code

---
+++ TestValidationJuly2018/TestValidationJuly2018/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestValidationJuly2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // write a program that validates zip codes 
+            Console.WriteLine("Please enter a valid zip code");
+            string input = Console.ReadLine();
+
+            //if (Regex.IsMatch(input, @"^\d{5}$"))
+            //{
+            //    Console.WriteLine("You entered a valid zip code!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Nice try! next time please do it right!");
+            //}
+
+            while (!Regex.IsMatch(input, @"^\d{5}$"))
+            {
+                Console.WriteLine("Nice try! next time please do it right!");
+                input = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("You entered a valid zip code!");
+
+
+        }
+    }
+}
