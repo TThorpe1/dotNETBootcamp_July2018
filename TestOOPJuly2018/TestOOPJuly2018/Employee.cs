57d41dee341824ed4ca70706897df3232cc09bcb
kamel-rush <kamel@grandcircus.co>
2018-07-30 14:10:54 -0400

    OOP

---
+++ TestOOPJuly2018/TestOOPJuly2018/Employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestOOPJuly2018
+{
+    class Employee
+    {
+        #region Fields
+        // Fields to represent the data (private), camelCase 
+        private string firstName;
+        private string lastName;
+        private float salary;
+        private string ssn;
+        #endregion
+
+        #region Properties
+        // Properties to protect the fields + validation (Regex)
+        // PascalCase 
+        public string FirstName
+        {
+            set { firstName = value;  }
+            get { return firstName; }
+        }
+
+        public string LastName
+        {
+            set { lastName = value;  }
+            get { return lastName; }
+
+        }
+
+        public string SSN
+        {
+            set {
+                if (!Regex.IsMatch(value, @"^\d{3}-\d{2}-\d{4}$\"))
+                {
+                    throw new Exception("SSN is not valid!");
+                }
+                else
+                {
+                    ssn = value;
+                }
+               
+            }
+            get { return ssn; }
+        }
+
+        public float Salary
+        {
+            set { salary = value; }
+            get { return salary; }
+
+        }
+         #endregion 
+
+        // Constructors 
+
+        // Methods 
+
+    }
+}
---
+++ TestOOPJuly2018/TestOOPJuly2018/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOOPJuly2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List of Employees 
+
+            List<Employee> employees = new List<Employee>();
+
+            // used to add new employees to the list 
+            while (true)
+            {
+                Employee temp = new Employee();
+                Console.WriteLine("Please provide the First Name:");
+                temp.FirstName = Console.ReadLine();
+
+                Console.WriteLine("Please provide the Last Name:");
+                temp.LastName = Console.ReadLine();
+
+                Console.WriteLine("Please provide the SSN:");
+                temp.SSN = Console.ReadLine();
+
+                Console.WriteLine("Please provide the Salary:");
+                temp.Salary = float.Parse(Console.ReadLine());
+
+                employees.Add(temp);// add the temp object to the employees list 
+
+                Console.WriteLine("Do you want to add more employees?");
+                string choice = Console.ReadLine();
+                if (choice == "N")
+                    break;  
+            }
+
+            foreach (Employee e in employees)
+            {
+
+                Console.WriteLine($"{e.FirstName} {e.LastName}");
+                Console.WriteLine("===================");
+            }
+
+            //for (int i = 0; i < employees.Count; i++)
+            //{
+            //    Console.WriteLine($"{employees[i].FirstName} {employees[i].LastName}");
+            //    Console.WriteLine("===================");
+            //}
+
+
+            //Employee john = new Employee();// object 
+
+            //john.FirstName = "John";// set
+
+            //Console.WriteLine(john.FirstName);//get 
+
+            //john.Salary = 50000;// set 
+
+            //john.Salary = john.Salary + 1000; // get then a set 
+
+            //Console.WriteLine(john.SSN);// blank 
+
+
+            
+
+
+
+
+
+
+        }
+    }
+}
