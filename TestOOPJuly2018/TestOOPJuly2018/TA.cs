4c682ed08eedae5c87c0df7f06fc24dd1f2554c3
kamel-rush <kamel@grandcircus.co>
2018-07-31 18:16:52 -0400

    OOP - polymorphism

---
+++ TestOOPJuly2018/TestOOPJuly2018/TA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOOPJuly2018
+{
+    class TA:Employee
+    {
+        private int workLoad;
+        public int WorkLoad
+        {
+            set { workLoad = value; }
+            get { return workLoad; }
+
+        }
+        public TA(string fn, string ln, float sal, string s, int wl) : base(fn, ln, sal, s)
+        {
+            WorkLoad = wl; 
+
+        }
+
+        public TA() : base()
+        {
+            WorkLoad = 30; 
+        }
+
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"WorkLoad {WorkLoad}");
+        }
+    }
+}
